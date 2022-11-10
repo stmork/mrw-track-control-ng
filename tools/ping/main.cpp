4e7d08430fe5be2f255e1d5cbef967cb99bc1c92
Steffen A. Mork <linux-dev@morknet.de>
2022-11-10 21:54:57 +0100

    First contact to CAN Bus via Qt API.

--- tools/ping/main.cpp
+++ tools/ping/main.cpp
@@ -7,15 +7,18 @@
 #include <QCanBus>
 #include <QDebug>
 
+#include <model/commands.h>
+
+using namespace mrw::model;
+
 int main(int argc, char * argv[])
 {
-	QCoreApplication a(argc, argv);
-	QCanBus * can_bus = QCanBus::instance();
+	QCoreApplication app(argc, argv);
+	QCanBus     *    can_bus = QCanBus::instance();
+	QString          error;
 
 	for (const QString & plugin : can_bus->plugins())
 	{
-		QString error;
-
 		qInfo().noquote() << plugin;
 		for (const QCanBusDeviceInfo & device : can_bus->availableDevices(plugin, &error))
 		{
@@ -23,5 +26,23 @@ int main(int argc, char * argv[])
 		}
 	}
 
-	return a.exec();
+	QCanBusDevice * device = can_bus->createDevice("socketcan", "can0", &error);
+
+	QObject::connect(device, &QCanBusDevice::framesReceived, [device]()
+	{
+		for (const QCanBusFrame & frame : device->readAllFrames())
+		{
+			qInfo() << frame.frameId() << frame.payload();
+		}
+	});
+
+	QByteArray payload;
+
+	payload += Command::PING;
+	device->connectDevice();
+
+	QCanBusFrame    frame(0x7ffu, payload);
+	device->writeFrame(frame);
+
+	return app.exec();
 }
