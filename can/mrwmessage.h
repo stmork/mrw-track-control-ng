f1fddc7601141a478f57281b90aaa5efc9faf74f
Steffen A. Mork <linux-dev@morknet.de>
2022-11-14 15:06:18 +0100

    Doxygen.

--- can/mrwmessage.h
+++ can/mrwmessage.h
@@ -28,12 +28,13 @@ namespace mrw::can
 	 * 1. As a command to be sent.
 	 * 2. As a response from a previous command.
 	 *
-	 * A command may address all Controller instances using the broadcast ID
-	 * CAN_BROADCAST_ID. In case a Device is addressed both is needed: the
-	 * Controller ID and the Device unit number.
+	 * A command may address all mrw::model::Controller instances using the
+	 * broadcast ID CAN_BROADCAST_ID. In case a mrw::model::Device is addressed
+	 * both is needed: the mrw::model::Controller ID and the mrw::model::Device
+	 * unit number.
 	 *
-	 * @see Controller:id()
-	 * @see Device::unitNo()
+	 * @see mrw::model::Controller:id()
+	 * @see mrw::model::Device::unitNo()
 	 */
 	class MrwMessage : public mrw::util::String
 	{
@@ -73,11 +74,12 @@ namespace mrw::can
 
 		/**
 		 * This constructor creates a command MrwMessage using an extended
-		 * frame format to address a specific Controller and Device.
+		 * frame format to address a specific mrw::model::Controller and
+		 * mrw::model::Device.
 		 *
 		 * @param command The command to process.
-		 * @param id The Controller to address by its ID.
-		 * @param no The Device to address by its unit number.
+		 * @param id The mrw::model::Controller to address by its ID.
+		 * @param no The mrw::model::Device to address by its unit number.
 		 */
 		explicit MrwMessage(
 			const Command                   command,
@@ -88,8 +90,9 @@ namespace mrw::can
 		 * This constructor creates a response MrwMessage using always an
 		 * extended frame format.
 		 *
-		 * @param id The addressed Controller, mostly the CAN gateway ID.
-		 * @param no The sending Device unit number.
+		 * @param id The addressed mrw::model::Controller, mostly the CAN
+		 * gateway ID.
+		 * @param no The sending mrw::model::Device unit number.
 		 * @param command The processed Command.
 		 * @param code The Response code.
 		 */
@@ -98,6 +101,14 @@ namespace mrw::can
 			const mrw::model::UnitNo        no,
 			const Command                   command,
 			const Response        code);
+
+		/**
+		 * This constructor parses a receive CAN frame. Depending on the
+		 * contents the resulting MrwMessage may be invalid.
+		 *
+		 * @param frame The CAN frame to parse
+		 * @see valid()
+		 */
 		explicit MrwMessage(const QCanBusFrame & frame);
 
 		uint16_t eid() const;
@@ -105,10 +116,10 @@ namespace mrw::can
 		quint32  id() const;
 
 		/**
-		 * This method returns the Device unit number. In case of a broadcast
-		 * MrwMessage this unit number is \c 0.
+		 * This method returns the mrw::model::Device unit number. In case of a
+		 * broadcast MrwMessage this unit number is \c 0.
 		 *
-		 * @return
+		 * @return The used mrw::model::Device unit number.
 		 */
 		inline mrw::model::UnitNo unitNo() const
 		{
@@ -117,16 +128,25 @@ namespace mrw::can
 
 		/**
 		 * This method returns the command inside the MrwMessage. If it is
-		 * a response the response flag (Response::CMD_RESPONSE) is
+		 * a response the response flag (@c Response::CMD_RESPONSE) is
 		 * already cleared.
 		 *
 		 * @return The command inside this MrwMessage
+		 * @see Command
 		 */
 		inline Command command() const
 		{
 			return msg_command;
 		}
 
+		/**
+		 * This method returns the MrwMessage response code in case of a
+		 * response. In case of a command message this method returns
+		 * @c Response::MSG_NO_RESPONSE.
+		 *
+		 * @return The MrwMessage response code
+		 * @see Response
+		 */
 		inline Response response() const
 		{
 			return msg_response;
