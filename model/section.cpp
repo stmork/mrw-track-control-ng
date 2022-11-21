7e74d5892e96a9105134065d6079fba6fe389114
Steffen A. Mork <linux-dev@morknet.de>
2022-11-21 12:06:47 +0100

    Formatted section output.

--- model/section.cpp
+++ model/section.cpp
@@ -170,7 +170,7 @@ void Section::findSignalPair()
 
 QString Section::toString() const
 {
-	return QString::asprintf("    Sec %c %4d: ", valid() ? 'V' : '-', unitNo()) + name();
+	return QString::asprintf("    Sec %c %5d  : ", valid() ? 'V' : '-', unitNo()) + name();
 }
 
 SectionModule * Section::resolve(const std::string & path)
