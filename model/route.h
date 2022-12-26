cc4f8c22255599e2606c292f978dd983fb0ee249
Steffen A. Mork <linux-dev@morknet.de>
2022-12-26 16:29:24 +0100

    Doxygen.

--- model/route.h
+++ model/route.h
@@ -18,6 +18,19 @@
 
 namespace mrw::model
 {
+	/**
+	 * This class represents a track of interconnected RailPart elements for
+	 * driving a train. It may extended at the back end to prolong the track
+	 * and shortened if the train as passed or left the RailPart elements.
+	 * It also computes the Signal symbol elements. The SignalControllerProxy
+	 * computes the definitive SignalState to show. The Route class ensures
+	 * that the containing RailPart elements could not allocated by other
+	 * Route instances using the RailPart::reserve() method. This class also
+	 * maintains the SectionState states of each involved Section.
+	 *
+	 * @see RailPart::reserve()
+	 * @see SectionState
+	 */
 	class Route : public QObject
 	{
 		Q_OBJECT
@@ -33,19 +46,63 @@ namespace mrw::model
 			QObject       *      parent = nullptr);
 		virtual ~Route();
 
+		/**
+		 * This cast operator returns the RailPart list for unit testing
+		 * purposes.
+		 */
 		inline operator const RailTrack & () const
 		{
 			return track;
 		}
 
+		/**
+		 * This method tries to find a prolonging track to the given RailPart.
+		 *
+		 * @param target The target RailPart to prolong this Route.
+		 * @return True if a track was successfully prolonged.
+		 */
 		bool append(RailPart * target);
+
+		/**
+		 * This method tries to find a RailPart track from the given RailPart
+		 * rail to the target parameter. If the search_region is not @c nullptr
+		 * The path between both RailPart ends are not allowed to leave the
+		 * given Region.
+		 *
+		 * @param rail The starting point.
+		 * @param target The end point.
+		 * @param search_region The Region where to search the track. May be
+		 * @c nullptr.
+		 * @return True if a track was successfully prolonged.
+		 */
 		bool append(
 			RailPart * rail,
 			RailPart * target,
 			Region  *  search_region);
+
+		/**
+		 * This method deallocates all contained Section and RailPart from
+		 * this instance.
+		 */
 		void clear();
+
+		/**
+		 * This method returns true if the last RailPart ends so that the
+		 * Route cannot be prolonged. It also ensures that the last segment
+		 * is turned.
+		 *
+		 * @return True if the last Rail is ended.
+		 */
 		bool isLastSectionEnded() const;
 
+		/**
+		 * This method tries to find neigbours. If there are no neighbours
+		 * the given RailPart ends there.
+		 *
+		 * @param neighbours The neighbours to collect.
+		 * @param part The RailPart to search from.
+		 * @param direction The counting direction to follow.
+		 */
 		static void isLastSectionEnded(
 			std::unordered_set<Section *> & neighbours,
 			const RailPart         *        part,
@@ -55,14 +112,41 @@ namespace mrw::model
 		virtual void dump() const;
 
 	protected:
+		/** The RailPart track to follow. */
 		RailTrack               track;
+
+		/** The Section track to follow. */
 		SectionTrack            sections;
-		Section        *        last_section  = nullptr;
+
+		/** The front end of the Section track. */
 		Section        *        first_section = nullptr;
+
+		/** The back end of the Section track. */
+		Section        *        last_section  = nullptr;
+
+		/** The allocation state. */
 		const SectionState      state = FREE;
+
+		/** The direction to drive. */
 		const bool              direction;
+
+		/**
+		 * State for automatic unblocking. This is necessary for
+		 * SectionState::TOUR mode.
+		 */
 		const bool              auto_unblock = false;
 
+		/**
+		 * This method prepares the RailPart track and initializes the
+		 * Section track and Signal::Symbol of each Section. Override this
+		 * method to additionally add QObject connections or update more
+		 * logic.
+		 *
+		 * @note Do not forget to call this base method in case of overloading!
+		 *
+		 * @param last_valid_section The last initialized Section.
+		 * @param last_valid_part The last initialized RailPart.
+		 */
 		virtual void prepare(
 			Section  * last_valid_section,
 			RailPart * last_valid_part);
