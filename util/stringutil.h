8326ae09228eb966532c70036c7d154c3fdc986c
Steffen A. Mork <linux-dev@morknet.de>
2023-01-10 18:31:56 +0100

    Doxygen.

--- util/stringutil.h
+++ util/stringutil.h
@@ -19,7 +19,13 @@
 #	define SKIP_EMPTY_PARTS QString::SkipEmptyParts
 #endif
 
-
+/**
+ * The mrw::util namespace contains some classes to make live easier. ;-)
+ *
+ * It contains string preparation classes, signal handlers, logging support
+ * and some other little stuff like random number generation or a simple
+ * constant to string mapper.
+ */
 namespace mrw::util
 {
 	/**
