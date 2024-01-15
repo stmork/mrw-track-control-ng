1512d1a1e83ad61c3dfe141c4bc035bc895bc5fa
Steffen A. Mork <linux-dev@morknet.de>
2024-01-15 13:41:15 +0100

    Year bump.

--- util/termhandler.cpp
+++ util/termhandler.cpp
@@ -1,6 +1,6 @@
 //
 //  SPDX-License-Identifier: MIT
-//  SPDX-FileCopyrightText: Copyright (C) 2008-2023 Steffen A. Mork
+//  SPDX-FileCopyrightText: Copyright (C) 2008-2024 Steffen A. Mork
 //
 
 #include <QCoreApplication>
