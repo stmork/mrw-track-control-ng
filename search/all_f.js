8efd95635dac77dbb4a7f9ec4ac672b4261af78c
stmork <stmork@users.noreply.github.com>
2025-04-06 08:57:50 +0000

    deploy: 42c8393ea13ca5937feca66b9245c6e4311beb0f

--- search/all_f.js
+++ search/all_f.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['qrybuf_0',['QRYBUF',['../d9/d9b/namespacemrw_1_1can.html#ae9a2e6e9bc528c9c7f38b7d26457d4fba0dcb905c0156a8196e593254fc94725e',1,'mrw::can']]],
-  ['qryerr_1',['QRYERR',['../d9/d9b/namespacemrw_1_1can.html#ae9a2e6e9bc528c9c7f38b7d26457d4fba005637f307ae952ebb0b21d23d237ccd',1,'mrw::can']]],
-  ['qtstatechart_2',['QtStatechart',['../d4/dfe/classmrw_1_1statechart_1_1QtStatechart.html',1,'mrw::statechart::QtStatechart&lt; T &gt;'],['../d4/dfe/classmrw_1_1statechart_1_1QtStatechart.html#a3e7ef6e2d57177d1e45464548789f87b',1,'mrw::statechart::QtStatechart::QtStatechart()']]],
-  ['quarter_3',['QUARTER',['../db/d4f/classmrw_1_1model_1_1Position.html#af838864426b2ae5e3f53e29298d3bdcc',1,'mrw::model::Position']]]
+  ['q_5fdeclare_5flogging_5fcategory_0',['Q_DECLARE_LOGGING_CATEGORY',['../d9/dbc/namespacemrw_1_1model.html#ac548114cf581332986747fa11a3ad3b8',1,'mrw::model']]],
+  ['qrybuf_1',['QRYBUF',['../d9/d9b/namespacemrw_1_1can.html#ae9a2e6e9bc528c9c7f38b7d26457d4fba0dcb905c0156a8196e593254fc94725e',1,'mrw::can']]],
+  ['qryerr_2',['QRYERR',['../d9/d9b/namespacemrw_1_1can.html#ae9a2e6e9bc528c9c7f38b7d26457d4fba005637f307ae952ebb0b21d23d237ccd',1,'mrw::can']]],
+  ['qtstatechart_3',['QtStatechart',['../d4/dfe/classmrw_1_1statechart_1_1QtStatechart.html',1,'mrw::statechart::QtStatechart&lt; T &gt;'],['../d4/dfe/classmrw_1_1statechart_1_1QtStatechart.html#a3e7ef6e2d57177d1e45464548789f87b',1,'mrw::statechart::QtStatechart::QtStatechart()']]],
+  ['quarter_4',['QUARTER',['../db/d4f/classmrw_1_1model_1_1Position.html#af838864426b2ae5e3f53e29298d3bdcc',1,'mrw::model::Position']]]
 ];
--- search/functions_f.js
+++ search/functions_f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['qtstatechart_0',['QtStatechart',['../d4/dfe/classmrw_1_1statechart_1_1QtStatechart.html#a3e7ef6e2d57177d1e45464548789f87b',1,'mrw::statechart::QtStatechart']]]
+  ['q_5fdeclare_5flogging_5fcategory_0',['Q_DECLARE_LOGGING_CATEGORY',['../d9/dbc/namespacemrw_1_1model.html#ac548114cf581332986747fa11a3ad3b8',1,'mrw::model']]],
+  ['qtstatechart_1',['QtStatechart',['../d4/dfe/classmrw_1_1statechart_1_1QtStatechart.html#a3e7ef6e2d57177d1e45464548789f87b',1,'mrw::statechart::QtStatechart']]]
 ];
