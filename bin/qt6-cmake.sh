8c1619486d887afca5609a1b10722f8c9178bd2e
Steffen A. Mork <linux-dev@morknet.de>
2025-04-13 11:21:31 +0200

    Added cmake support.

---
+++ bin/qt6-cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+export PREFIX=$PWD/rootfs/${PACKAGE}
+export DEB_CONTROL=control-ng-qt6
+
+BUILD_DIR=build
+ARCH_TYPE=`dpkg-architecture -qDEB_HOST_GNU_TYPE`
+
+rm -rf ${BUILD_DIR} ${PREFIX}
+cmake \
+   -DQT_HOST_PATH=/ \
+   -DQT_HOST_PATH_CMAKE_DIR=/usr/lib/${ARCH_TYPE}/cmake \
+   -DCMAKE_TOOLCHAIN_FILE=$1 \
+   -B ${BUILD_DIR}
+cmake --build ${BUILD_DIR} --parallel `nproc`
+cmake --install ${BUILD_DIR} --config Release
+
+. $PWD/bin/debian.sh
