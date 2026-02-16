3933cc0a7bed1177ebe98eccc55cc165d799f739
stmork <stmork@users.noreply.github.com>
2026-02-16 12:30:05 +0000

    deploy: 84a303c015cdeef4c5835ed5f9dfd4874836aa01

--- search/all_13.js
+++ search/all_13.js
@@ -7,8 +7,9 @@ var searchData=
   ['unlocked_4',['UNLOCKED',['../d8/d2c/classmrw_1_1model_1_1Device.html#a5aeac6a51c26164ebb6964d807d5c3a4a298a2da567ae581f8886ef8d533107f4',1,'mrw::model::Device']]],
   ['unregisterlogger_5',['unregisterLogger',['../d4/dc7/classmrw_1_1log_1_1LoggerService.html#a54b982f05b8b31b9c19dbb7852465f41',1,'mrw::log::LoggerService']]],
   ['unsettimer_6',['unsetTimer',['../df/d18/classsc_1_1timer_1_1TimerServiceInterface.html#a43f25c75a536b85d7f0d0b2196c7d1b6',1,'sc::timer::TimerServiceInterface']]],
-  ['update_7',['update',['../d8/db5/classmrw_1_1ctrl_1_1BaseController.html#ab260bbe7258e2143720d0e6f04901d5b',1,'mrw::ctrl::BaseController']]],
-  ['usedpins_8',['usedPins',['../d6/d71/classmrw_1_1model_1_1LightSignal.html#ab0ede4577a1723fae75b43891617ff32',1,'mrw::model::LightSignal']]],
-  ['user_5frole_9',['USER_ROLE',['../db/d76/classmrw_1_1ui_1_1ControllerWidget.html#a14ccbb61b1637fa8740662f1cec226b5',1,'mrw::ui::ControllerWidget']]],
-  ['ushort_10',['ushort',['../db/d03/namespacesc.html#a6fd8c6bf037ef38d61541e1d27ddf532',1,'sc']]]
+  ['unsettimerraw_7',['unsetTimerRaw',['../df/d18/classsc_1_1timer_1_1TimerServiceInterface.html#a912f63105c142bd8c57deb3b320e0dcf',1,'sc::timer::TimerServiceInterface']]],
+  ['update_8',['update',['../d8/db5/classmrw_1_1ctrl_1_1BaseController.html#ab260bbe7258e2143720d0e6f04901d5b',1,'mrw::ctrl::BaseController']]],
+  ['usedpins_9',['usedPins',['../d6/d71/classmrw_1_1model_1_1LightSignal.html#ab0ede4577a1723fae75b43891617ff32',1,'mrw::model::LightSignal']]],
+  ['user_5frole_10',['USER_ROLE',['../db/d76/classmrw_1_1ui_1_1ControllerWidget.html#a14ccbb61b1637fa8740662f1cec226b5',1,'mrw::ui::ControllerWidget']]],
+  ['ushort_11',['ushort',['../db/d03/namespacesc.html#a6fd8c6bf037ef38d61541e1d27ddf532',1,'sc']]]
 ];
--- search/functions_13.js
+++ search/functions_13.js
@@ -3,6 +3,7 @@ var searchData=
   ['unitno_0',['unitNo',['../d8/d2c/classmrw_1_1model_1_1Device.html#a262cee73971200e5c6cd308a7621682f',1,'mrw::model::Device::unitNo()'],['../d1/d80/classmrw_1_1can_1_1MrwMessage.html#a92096a014295e7c48f83d69917df0969',1,'mrw::can::MrwMessage::unitNo()']]],
   ['unregisterlogger_1',['unregisterLogger',['../d4/dc7/classmrw_1_1log_1_1LoggerService.html#a54b982f05b8b31b9c19dbb7852465f41',1,'mrw::log::LoggerService']]],
   ['unsettimer_2',['unsetTimer',['../df/d18/classsc_1_1timer_1_1TimerServiceInterface.html#a43f25c75a536b85d7f0d0b2196c7d1b6',1,'sc::timer::TimerServiceInterface']]],
-  ['update_3',['update',['../d8/db5/classmrw_1_1ctrl_1_1BaseController.html#ab260bbe7258e2143720d0e6f04901d5b',1,'mrw::ctrl::BaseController']]],
-  ['usedpins_4',['usedPins',['../d6/d71/classmrw_1_1model_1_1LightSignal.html#ab0ede4577a1723fae75b43891617ff32',1,'mrw::model::LightSignal']]]
+  ['unsettimerraw_3',['unsetTimerRaw',['../df/d18/classsc_1_1timer_1_1TimerServiceInterface.html#a912f63105c142bd8c57deb3b320e0dcf',1,'sc::timer::TimerServiceInterface']]],
+  ['update_4',['update',['../d8/db5/classmrw_1_1ctrl_1_1BaseController.html#ab260bbe7258e2143720d0e6f04901d5b',1,'mrw::ctrl::BaseController']]],
+  ['usedpins_5',['usedPins',['../d6/d71/classmrw_1_1model_1_1LightSignal.html#ab0ede4577a1723fae75b43891617ff32',1,'mrw::model::LightSignal']]]
 ];
