1ca78b43cb83cb4b8098be69cbdf26629a68fb5c
stmork <stmork@users.noreply.github.com>
2026-05-17 19:50:57 +0000

    deploy: d4f02ed9c773c16c6293f69c750cb316011fcd75

--- search/all_7.js
+++ search/all_7.js
@@ -1,18 +1,19 @@
 var searchData=
 [
   ['half_0',['HALF',['../db/d4f/classmrw_1_1model_1_1Position.html#adc06dce2c59482cd16412ea864892b00',1,'mrw::model::Position']]],
-  ['has_5fdistant_1',['has_distant',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#a1f5b1bb1f6439c832ef2ab1ebd81e468',1,'mrw::ctrl::SignalController::Status']]],
-  ['has_5fflank_5fprotection_2',['has_flank_protection',['../dc/d79/structmrw_1_1ctrl_1_1BaseSwitchController_1_1Status.html#a33dc8379c42890008405241119aa2562',1,'mrw::ctrl::BaseSwitchController::Status']]],
-  ['has_5fmain_3',['has_main',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#af273dae34687e68fbbf624982da077e8',1,'mrw::ctrl::SignalController::Status']]],
-  ['has_5fshunting_4',['has_shunting',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#ae5a73594259c9e173495cbc6807dcf3f',1,'mrw::ctrl::SignalController::Status']]],
-  ['hascontroller_5',['hasController',['../d8/d2c/classmrw_1_1model_1_1Device.html#a1571a81a0f14ce06a49e24cdc85e42f8',1,'mrw::model::Device']]],
-  ['hascutoff_6',['hasCutOff',['../d6/d75/classmrw_1_1model_1_1SwitchModuleReference.html#a2c8e5c0e6dfdf1ec795ab52d23753448',1,'mrw::model::SwitchModuleReference']]],
-  ['haslock_7',['hasLock',['../df/db9/classmrw_1_1ui_1_1BaseWidget.html#a10e237ea1c94df68165b97e2509fecab',1,'mrw::ui::BaseWidget::hasLock()'],['../d8/d02/classmrw_1_1ui_1_1DoubleCrossSwitchWidget.html#ab9e59f5cfda7ceab33b437eedd1502c9',1,'mrw::ui::DoubleCrossSwitchWidget::hasLock()'],['../d5/dd0/classmrw_1_1ui_1_1RegularSwitchWidget.html#a164d22f7d6244f4fc7ce7b490558543b',1,'mrw::ui::RegularSwitchWidget::hasLock()'],['../d8/df9/classmrw_1_1ui_1_1SignalWidget.html#a7b749d27e73b6f8a2c3b8683840d8f8f',1,'mrw::ui::SignalWidget::hasLock()']]],
-  ['height_8',['height',['../db/d4f/classmrw_1_1model_1_1Position.html#a323e205493b37dae99dc97939f096c8a',1,'mrw::model::Position']]],
-  ['hexline_9',['HexLine',['../d0/d55/classmrw_1_1util_1_1HexLine.html',1,'mrw::util::HexLine'],['../d0/d55/classmrw_1_1util_1_1HexLine.html#a855c36b3062227265b35790c3e722f78',1,'mrw::util::HexLine::HexLine()=delete'],['../d0/d55/classmrw_1_1util_1_1HexLine.html#ade53720a08e5caf79458ecf6f0e14f79',1,'mrw::util::HexLine::HexLine(const QString &amp;input)']]],
-  ['hostname_10',['hostname',['../d9/d33/classmrw_1_1util_1_1AppSupport.html#af19bde96d35a484b351274db8a1bebbf',1,'mrw::util::AppSupport']]],
-  ['hz1_11',['Hz1',['../db/d8f/classmrw_1_1util_1_1ClockService.html#aa097bc71ce6a1a2af55ac34af02d377b',1,'mrw::util::ClockService']]],
-  ['hz2_12',['Hz2',['../db/d8f/classmrw_1_1util_1_1ClockService.html#aff9d02e4d105be76c2f683213037b87e',1,'mrw::util::ClockService']]],
-  ['hz4_13',['Hz4',['../db/d8f/classmrw_1_1util_1_1ClockService.html#ab987466889f56f4be79d75761e36a019',1,'mrw::util::ClockService']]],
-  ['hz8_14',['Hz8',['../db/d8f/classmrw_1_1util_1_1ClockService.html#a46b81845210ba4150aec594ce3bd9606',1,'mrw::util::ClockService']]]
+  ['has_5fcrossing_1',['has_crossing',['../db/d92/structmrw_1_1ctrl_1_1RailController_1_1Status.html#a1b33a032378c455c1fc54e3b4742b603',1,'mrw::ctrl::RailController::Status']]],
+  ['has_5fdistant_2',['has_distant',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#a1f5b1bb1f6439c832ef2ab1ebd81e468',1,'mrw::ctrl::SignalController::Status']]],
+  ['has_5fflank_5fprotection_3',['has_flank_protection',['../dc/d79/structmrw_1_1ctrl_1_1BaseSwitchController_1_1Status.html#a33dc8379c42890008405241119aa2562',1,'mrw::ctrl::BaseSwitchController::Status']]],
+  ['has_5fmain_4',['has_main',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#af273dae34687e68fbbf624982da077e8',1,'mrw::ctrl::SignalController::Status']]],
+  ['has_5fshunting_5',['has_shunting',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#ae5a73594259c9e173495cbc6807dcf3f',1,'mrw::ctrl::SignalController::Status']]],
+  ['hascontroller_6',['hasController',['../d8/d2c/classmrw_1_1model_1_1Device.html#a1571a81a0f14ce06a49e24cdc85e42f8',1,'mrw::model::Device']]],
+  ['hascutoff_7',['hasCutOff',['../d6/d75/classmrw_1_1model_1_1SwitchModuleReference.html#a2c8e5c0e6dfdf1ec795ab52d23753448',1,'mrw::model::SwitchModuleReference']]],
+  ['haslock_8',['hasLock',['../df/db9/classmrw_1_1ui_1_1BaseWidget.html#a10e237ea1c94df68165b97e2509fecab',1,'mrw::ui::BaseWidget::hasLock()'],['../d8/d02/classmrw_1_1ui_1_1DoubleCrossSwitchWidget.html#ab9e59f5cfda7ceab33b437eedd1502c9',1,'mrw::ui::DoubleCrossSwitchWidget::hasLock()'],['../d5/dd0/classmrw_1_1ui_1_1RegularSwitchWidget.html#a164d22f7d6244f4fc7ce7b490558543b',1,'mrw::ui::RegularSwitchWidget::hasLock()'],['../d8/df9/classmrw_1_1ui_1_1SignalWidget.html#a7b749d27e73b6f8a2c3b8683840d8f8f',1,'mrw::ui::SignalWidget::hasLock()']]],
+  ['height_9',['height',['../db/d4f/classmrw_1_1model_1_1Position.html#a323e205493b37dae99dc97939f096c8a',1,'mrw::model::Position']]],
+  ['hexline_10',['HexLine',['../d0/d55/classmrw_1_1util_1_1HexLine.html',1,'mrw::util::HexLine'],['../d0/d55/classmrw_1_1util_1_1HexLine.html#a855c36b3062227265b35790c3e722f78',1,'mrw::util::HexLine::HexLine()=delete'],['../d0/d55/classmrw_1_1util_1_1HexLine.html#ade53720a08e5caf79458ecf6f0e14f79',1,'mrw::util::HexLine::HexLine(const QString &amp;input)']]],
+  ['hostname_11',['hostname',['../d9/d33/classmrw_1_1util_1_1AppSupport.html#af19bde96d35a484b351274db8a1bebbf',1,'mrw::util::AppSupport']]],
+  ['hz1_12',['Hz1',['../db/d8f/classmrw_1_1util_1_1ClockService.html#aa097bc71ce6a1a2af55ac34af02d377b',1,'mrw::util::ClockService']]],
+  ['hz2_13',['Hz2',['../db/d8f/classmrw_1_1util_1_1ClockService.html#aff9d02e4d105be76c2f683213037b87e',1,'mrw::util::ClockService']]],
+  ['hz4_14',['Hz4',['../db/d8f/classmrw_1_1util_1_1ClockService.html#ab987466889f56f4be79d75761e36a019',1,'mrw::util::ClockService']]],
+  ['hz8_15',['Hz8',['../db/d8f/classmrw_1_1util_1_1ClockService.html#a46b81845210ba4150aec594ce3bd9606',1,'mrw::util::ClockService']]]
 ];
--- search/variables_7.js
+++ search/variables_7.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['half_0',['HALF',['../db/d4f/classmrw_1_1model_1_1Position.html#adc06dce2c59482cd16412ea864892b00',1,'mrw::model::Position']]],
-  ['has_5fdistant_1',['has_distant',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#a1f5b1bb1f6439c832ef2ab1ebd81e468',1,'mrw::ctrl::SignalController::Status']]],
-  ['has_5fflank_5fprotection_2',['has_flank_protection',['../dc/d79/structmrw_1_1ctrl_1_1BaseSwitchController_1_1Status.html#a33dc8379c42890008405241119aa2562',1,'mrw::ctrl::BaseSwitchController::Status']]],
-  ['has_5fmain_3',['has_main',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#af273dae34687e68fbbf624982da077e8',1,'mrw::ctrl::SignalController::Status']]],
-  ['has_5fshunting_4',['has_shunting',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#ae5a73594259c9e173495cbc6807dcf3f',1,'mrw::ctrl::SignalController::Status']]]
+  ['has_5fcrossing_1',['has_crossing',['../db/d92/structmrw_1_1ctrl_1_1RailController_1_1Status.html#a1b33a032378c455c1fc54e3b4742b603',1,'mrw::ctrl::RailController::Status']]],
+  ['has_5fdistant_2',['has_distant',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#a1f5b1bb1f6439c832ef2ab1ebd81e468',1,'mrw::ctrl::SignalController::Status']]],
+  ['has_5fflank_5fprotection_3',['has_flank_protection',['../dc/d79/structmrw_1_1ctrl_1_1BaseSwitchController_1_1Status.html#a33dc8379c42890008405241119aa2562',1,'mrw::ctrl::BaseSwitchController::Status']]],
+  ['has_5fmain_4',['has_main',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#af273dae34687e68fbbf624982da077e8',1,'mrw::ctrl::SignalController::Status']]],
+  ['has_5fshunting_5',['has_shunting',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#ae5a73594259c9e173495cbc6807dcf3f',1,'mrw::ctrl::SignalController::Status']]]
 ];
