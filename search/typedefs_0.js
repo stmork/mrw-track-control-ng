aaa57b9a4833fcbc97040e41fa870c3e98c26b0e
stmork <stmork@users.noreply.github.com>
2026-05-19 10:48:46 +0000

    deploy: d062cd7b3f76b44b5d50cc9ac89cfc6f01f6ecc4

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['constantmappair_0',['ConstantMapPair',['../d3/d25/classmrw_1_1util_1_1ConstantEnumerator.html#a2fb8988f103ebc51fe1395b9d553182a',1,'mrw::util::ConstantEnumerator']]],
-  ['controllerid_1',['ControllerId',['../d9/d9b/namespacemrw_1_1can.html#ad3397ba650ef1dbb629a49e0d5f78ba8',1,'mrw::can']]]
+  ['controllerid_1',['ControllerId',['../d9/d9b/namespacemrw_1_1can.html#ad3397ba650ef1dbb629a49e0d5f78ba8',1,'mrw::can']]],
+  ['crossingid_2',['CrossingId',['../d9/dbc/namespacemrw_1_1model.html#a5eac39705c21623d157d175879fd9c60',1,'mrw::model']]]
 ];
