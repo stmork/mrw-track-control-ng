b83b3f2c5c9b23300c086521150a2178bea632bf
stmork <stmork@users.noreply.github.com>
2026-05-20 19:00:38 +0000

    deploy: 65eadc0d879510dd356d3aa662a9d5f72b3a551c

--- search/variables_2.js
+++ search/variables_2.js
@@ -8,7 +8,7 @@ var searchData=
   ['can_5fsid_5fmask_5',['CAN_SID_MASK',['../d9/d9b/namespacemrw_1_1can.html#a5bc2dc75cc9bbe49df30ba7ecde4ff7b',1,'mrw::can']]],
   ['can_5fsid_5fshift_6',['CAN_SID_SHIFT',['../d9/d9b/namespacemrw_1_1can.html#a09d78d50b144919ce41199f02a0cc9d4',1,'mrw::can']]],
   ['connector_5flist_7',['connector_list',['../db/d76/classmrw_1_1ui_1_1ControllerWidget.html#a7e7621b6aea5332d8fc002cdda357178',1,'mrw::ui::ControllerWidget']]],
-  ['counter_8',['counter',['../db/d4f/classmrw_1_1model_1_1Position.html#a4d7793ec6944ee1f89dec89cc0ea8a12',1,'mrw::model::Position']]],
+  ['counter_8',['counter',['../db/d4f/classmrw_1_1model_1_1Position.html#a4d7793ec6944ee1f89dec89cc0ea8a12',1,'mrw::model::Position::counter'],['../db/d76/classmrw_1_1ui_1_1ControllerWidget.html#a45624ef3fd3fd765c740ef5dbf0e36f3',1,'mrw::ui::ControllerWidget::counter']]],
   ['crx_5fname_9',['crx_name',['../d6/d5c/classmrw_1_1model_1_1Crossing.html#a1ac759ca4b27a392d3630bc0d81ac57e',1,'mrw::model::Crossing']]],
   ['cyan_5fon_10',['CYAN_ON',['../d0/d55/classmrw_1_1util_1_1String.html#a8026cc002373c31b4dc8193a2a1eb599',1,'mrw::util::String']]]
 ];
