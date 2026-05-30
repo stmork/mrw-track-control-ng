877035d5c5878115f43b3edfe9069467b8bcdfb7
stmork <stmork@users.noreply.github.com>
2026-05-30 11:03:21 +0000

    deploy: 19e02efec6d59b9f31b1af4c853518acd22f1a54

--- search/variables_3.js
+++ search/variables_3.js
@@ -6,7 +6,9 @@ var searchData=
   ['distant_5fcolor_3',['distant_color',['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a31fcdb6253653185b6d7c7c72a45f95f',1,'mrw::ui::SignalWidget::Status']]],
   ['distant_5fstate_4',['distant_state',['../d5/df8/structmrw_1_1ctrl_1_1SignalController_1_1Status.html#a380bd08548dca28214fdda37f9dcfc41',1,'mrw::ctrl::SignalController::Status']]],
   ['do_5fbend_5',['do_bend',['../de/d01/structmrw_1_1ui_1_1RailWidget_1_1Status.html#a2ab1a9d884bd787abb7181109cb64966',1,'mrw::ui::RailWidget::Status::do_bend'],['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a1746bb8334c16c3534680d565356a976',1,'mrw::ui::SignalWidget::Status::do_bend']]],
-  ['draw_5fdistant_6',['draw_distant',['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a4e2055f578c3cbccd22f5d58b80dfe72',1,'mrw::ui::SignalWidget::Status']]],
-  ['draw_5flock_7',['draw_lock',['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a5befd09d4b286ad751efe0e0870cba0e',1,'mrw::ui::SignalWidget::Status']]],
-  ['draw_5fshunt_8',['draw_shunt',['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a77f8f9b723f689f4eccf8fe8cbb1d274',1,'mrw::ui::SignalWidget::Status']]]
+  ['draw_5fcrossing_5fafter_6',['draw_crossing_after',['../de/d01/structmrw_1_1ui_1_1RailWidget_1_1Status.html#a548ac1b17e71b3f8621993cf75fbf0cc',1,'mrw::ui::RailWidget::Status']]],
+  ['draw_5fcrossing_5fbefore_7',['draw_crossing_before',['../de/d01/structmrw_1_1ui_1_1RailWidget_1_1Status.html#a8e7c794b2945c8924b445d6d2de01c14',1,'mrw::ui::RailWidget::Status']]],
+  ['draw_5fdistant_8',['draw_distant',['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a4e2055f578c3cbccd22f5d58b80dfe72',1,'mrw::ui::SignalWidget::Status']]],
+  ['draw_5flock_9',['draw_lock',['../de/d01/structmrw_1_1ui_1_1RailWidget_1_1Status.html#ab6091b95877f51acb9fa730a7e22b58b',1,'mrw::ui::RailWidget::Status::draw_lock'],['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a5befd09d4b286ad751efe0e0870cba0e',1,'mrw::ui::SignalWidget::Status::draw_lock']]],
+  ['draw_5fshunt_10',['draw_shunt',['../d7/dc3/structmrw_1_1ui_1_1SignalWidget_1_1Status.html#a77f8f9b723f689f4eccf8fe8cbb1d274',1,'mrw::ui::SignalWidget::Status']]]
 ];
